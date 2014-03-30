a77c02919abeeb74496f715e79d797d841293756
bpluijms <bpluijms@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-30 12:12:12 +0000

    Updated to fix some errors
    git-svn-id: https://plugins.svn.wordpress.org/ticketheere/tags/0.2@884585 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/js/ticketheere.js
@@ -0,0 +1,14 @@
+var org=djh.org;
+var ev=djh.ev;
+var med=djh.med;
+var useTunnel = true;
+var tunnelurl = djh.tunnelUrl;
+
+  (function() {
+    var jh = document.createElement('script');
+    jh.type = 'text/javascript'; 
+    jh.async = true;
+    jh.src = 'https://shop.ticketheere.nl/js/integrate.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(jh, s);
+  })();
